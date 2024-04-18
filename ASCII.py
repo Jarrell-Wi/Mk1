28323ca0cb8bd5950ba9430f8493862552dcf652
Jarrell-Wi <156929253+Jarrell-Wi@users.noreply.github.com>
2024-04-18 07:32:04 +0000

    Removed Bloat

--- ASCII.py
+++ ASCII.py
@@ -7,9 +7,5 @@ for i in range(len(ASCII)):
     Binary.append(bin(ASCII[i]))
 for i in range(len(Binary)):
     Binary[i] = str(Binary[i])
-    Binary[i][0] = ''
-    Binary[i][1] = ''
-
-    print(ASCII[i])
-    print(Binary[i])
-    
+print(ASCII)
+print(Binary)
