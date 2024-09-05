5a2d6a829fd14cc07620d1c74d9f3acb82ce2387
Jarrell-Wi <156929253+Jarrell-Wi@users.noreply.github.com>
2024-09-05 13:22:22 +0000

    finished basics of calc

--- Calc.py
+++ Calc.py
@@ -4,55 +4,52 @@ def getequation (choice):
     for i in range(len(choice)):
         for j in range(len(operators)):
             if choice[i] == operators[j]:
-                op = j
+                op = operators[j]
+                opr = j
     equation = choice.split(str(op))
-    return(equation, op)
+    return(equation, opr)
 
 def  add (left, right):
-    answer = left + right
+    answer = int(left) + int(right)
     return(answer)
 
 def  subtract(left, right):
-    answer = left - right
+    answer = int(left) - int(right)
     return(answer)
 
 def  divide(left, right):
-    answer = left / right
+    answer = int(left) / int(right)
     return(answer)
 
 def  multiply(left, right):
-    answer = left * right
+    answer = int(left) * int(right)
     return(answer)
 
 def modulate  (left, right):
-    answer = left % right
+    answer = int(left) % int(right)
     return(answer)
 
+print('STOP to end')
+stay = True
+while stay == True:
+    choice = input('Enter Equation: ')
+    if choice == 'STOP':
+        break
+    calc = getequation(choice)
+    op = calc[1]
+    left = calc[0][0]
+    right = calc[0][1]
 
 
-
-
-
-
-
-
-
-
-
-
-choice = input('Gimme equation')
-calc = getequation(choice)
-left = calc[0][0]
-right = calc[0][1]
-op = calc[1][0]
-if op == 0:
-    result = add(left, right, op)
-elif op == 1:
-    result = subtract(left, right, op)
-elif op == 2:
-    result = divide(left, right, op)
-elif op == 3:
-    result = multiply(left, right, op)
-elif op == 4:
-    result = modulate(left, right, op)
-print(result)
+    if op == 0:
+        result = add(left, right)
+    elif op == 1:
+        result = subtract(left, right)
+    elif op == 2:
+        result = divide(left, right)
+    elif op == 3:
+        result = multiply(left, right)
+    elif op == 4:
+        result = modulate(left, right)
+    print('V')
+    print(result)
