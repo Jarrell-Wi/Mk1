6c4bd9ece00a8bacc0ed491b8a1e8e704b4bae7c
Jarrell-Wi <156929253+Jarrell-Wi@users.noreply.github.com>
2024-09-10 13:01:30 +0000

    develop save game state

--- A-Level/savetheprincess.py
+++ A-Level/savetheprincess.py
@@ -1,33 +1,49 @@
 from random import randint
-import random
 
 size=2
 while size%2==0:
     size=int(input("enter the grid size - must be an odd number: "))
 
-board=[["-" for x in range(size)] for y in range(size)]
+grid=[["-" for x in range(size)] for y in range(size)]
 mid=(size//2)
-board[mid][mid]="m"
-princessx = random.randint(0,size)
-princessy = random.randint(0,size)
-board [princessx][princessy] = 'p'
+grid[mid][mid]="m"
+grid[randint(0,size-1)][randint(0,size-1)]="p"
 
-
-
-
-
-
-for row in board:
+for row in grid:
     for e in row:
         print (e, end='')
     print()
 
 print("Maurizio needs to rescue the Princess")
 
-mariopos = mid
-for i in range(len(board)):
-    for j in range(len(row)):
+maurCoords = []
+prinCoords = []
 
+for i in range(len(grid)):
+    print(grid[i])
+    for j in range(len(grid[i])):
+        if grid[i][j] == "p":
+            prinCoords = [i, j]
+        if grid[i][j] == "m":
+            maurCoords = [i, j]
+
+if maurCoords[0] > prinCoords[0]:
+    coordValue = maurCoords[0] - prinCoords[0]
+    for i in range(coordValue):
+        print("UP")
+else:
+    coordValue = prinCoords[0] - maurCoords[0]
+    for i in range(coordValue):
+        print("DOWN")
+
+if maurCoords[1] > prinCoords[1]:
+    coordValue = maurCoords[1] - prinCoords[1]
+    for i in range(coordValue):
+        print("LEFT")
+else:
+    coordValue = prinCoords[1] - maurCoords[1]
+    for i in range(coordValue):
+        print("RIGHT")
 
 
 #you need to write the code to solve the puzzle
--- Projects/Bralfie Brant Simulator.py
+++ Projects/Bralfie Brant Simulator.py
@@ -1,5 +1,4 @@
 import random
-Game = True
 
 class Investment:
     def __init__(self, name):
@@ -18,6 +17,8 @@ class User:
         self.Money = 1000
         self.Investments = {}
         self.Day = 0
+        self.LocalSave = ""
+        self.Username = ""
     def Invest(self, selection, amount):
         currentStockPrice = selection.stockValue
         totalPrice = currentStockPrice * amount
@@ -39,11 +40,40 @@ WiggetGroup = Investment("Wigget Group")
 Investments = [McDonalds, BurgerKing, WiggetGroup]
 
 def CreateLocal():
-    with open("Data.txt") as file:
-        pass
+    num = random.randint(0, 999999)
+    text = ""
+    try:
+        with open("./UserSave"+Player.Username, "r+") as file:
+            text = file.readline()
+            file.close()
+    except:
+        open("./UserSave"+Player.Username, "x")
+    if text == "":
+        Player.LocalSave = "UserSave" + str(num)
+    else:
+        Player.LocalSave = text
+        print(Player.LocalSave)
 
 def SaveData():
-    pass
+    Data = Player.LocalSave + "\n" + str(Player.Money) + "\n" + str(Player.Investments) + "\n" + str(Player.Day)
+    with open("UserSave"+Player.Username, "w+") as file:
+        file.write(Data)
+        file.close()
+
+def LoadData():
+    player = ("./UserSave"+Player.Username)
+    try:
+       with open(player, "r+") as playerdata:
+        for i in playerdata:
+            print(i)
+
+
+
+
+        pass #Open File, Read Data, Set Data
+    except:
+        pass #Error
+
 
 def stockinvest():
     for i in range(len(Investments)):
@@ -51,10 +81,11 @@ def stockinvest():
     investment = int(input('where do you want to invest: '))
     investment = Investments[investment-1]
     print('Balance -', Player.Money)
-    amount = int(input('How much: '))
+    amount = float(input('How much: '))
     Player.Invest(investment, amount)
 
 def startday():
+    Game = True
     while Game != False:
         for i in range(len(Investments)):
             Investments[i].changeStock()
@@ -62,7 +93,13 @@ def startday():
         messages = [f'It\'s day {Player.Day} lets get started.', f'Rise and shine it\'s day {Player.Day} time to start investing.', f'GooPlayer.Day it\'s day {Player.Day}. Get the flippity dippity flop up and start investing YOU LAZY BASTARD!', 'Good morning sunshine, invest don\'t rest and get to it', f'Day {Player.Day} wow look how far we\'ve come, tired of investing yet? I DONT CARE DO YOU WANT YOUR LAPTOP FIXED OR NOT!','Get up! Your snooze button isn\'t going to press itself.',f'WAKE UP! Its day {Player.Day}, your pillow is begging for a break seriously.', f'Morning, it\'s day {Player.Day}! Your bed might miss you but the world won\'t. I promise.']
         choice = random.randint(0, len(messages) - 1)
         print(messages[choice])
-        stockinvest()
+        print("What Do You Want To Do?")
+        userChoice = input(">")
+        if userChoice == "exit":
+            Game = False
+        else:
+            stockinvest()
+    SaveData()
 
 def startgame():
     print('Welcome to Bralfie Brant Simulator the hit investment game!')
@@ -70,8 +107,11 @@ def startgame():
     print('so you can afford to pay to fix your super sick RGB gaming laptop')
     print('after it broke T-T. Good luck :D')
     print()
+    Username = input("Enter A Username: ")
+    Player.Username = Username
+    CreateLocal()
+    LoadData()
     startday()
 
 startgame()
-
 
