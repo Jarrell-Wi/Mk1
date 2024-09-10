b34d55d1548ae5a77e0d7e1211f769f48b4ec956
Jarrell-Wi <156929253+Jarrell-Wi@users.noreply.github.com>
2024-09-10 08:50:52 +0000

    added homework

---
+++ A-Level/savetheprincess.py
@@ -0,0 +1,39 @@
+from random import randint
+
+size=2
+while size%2==0:
+    size=int(input("enter the grid size - must be an odd number: "))
+
+board=[["-" for x in range(size)] for y in range(size)]
+mid=(size//2)
+board[mid][mid]="m"
+board[randint(0,size)][randint(0,size)]="p"
+
+mariopos = mid
+for i in range(len(board)):
+    for i in range(len(row)):
+
+
+
+
+for row in board:
+    for e in row:
+        print (e, end='')
+    print()
+
+print("Maurizio needs to rescue the Princess")
+
+
+
+
+#you need to write the code to solve the puzzle
+# It should output the directions Maurizio needs to take to get to Princess Pineapple
+# Directions should be output in the form "UP" "DOWN" "LEFT" "RIGHT"
+# e.g. "UP" "UP" "LEFT" "LEFT" "LEFT"
+
+
+#hints
+
+# use nested for loops to find the princesses row and column
+# calculate the difference between that co-ordinate and Maurizio
+# output the correct command set
