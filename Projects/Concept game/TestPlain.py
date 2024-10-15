3fb5a277a798d53fae2ef6261b99220557e041d7
Jarrell-Wi <156929253+Jarrell-Wi@users.noreply.github.com>
2024-10-15 13:28:31 +0000

    Develop Diagonal Scan function

--- Projects/Concept game/TestPlain.py
+++ Projects/Concept game/TestPlain.py
@@ -14,7 +14,7 @@ def CheckHor(Board, Player):
         for j in range (3):
             if Board[i][j] == Player:
                 prob +=1
-        full = 'Hor' + str(i + 1) + ' prob' + str(prob)
+        full = 'H' + str(i + 1) + '' + str(prob)
         outcomes.append(full)
     return outcomes
 
@@ -25,16 +25,50 @@ def CheckVer(Board, Player):
         for j in range(3):
             if Board[j][i] == Player:
                 prob += 1
-        full = 'Ver' + str(i + 1) + ' prob' + str(prob)
+        full = 'V' + str(i + 1) + 'prob' + str(prob)
         outcomes.append(full)
     return outcomes
-        
+
+
+def CheckDiag(Board, Player):
+    outcomes = []
+    prob = 0
+    x = 2
+    y = 0
+    for i in range(3):
+        if Board[i][i * (-1)] == Player:
+            prob += 1
+    full = 'DL prob' + str(prob)
+    outcomes.append(full)
+
+    prob = 0
+    for i in range(3):
+        if Board[x][y] == Player:
+            prob += 1
+        x -= 1
+        y += 1
+    full = 'DR prob' + str(prob)
+    outcomes.append(full)
+    return outcomes
+
+
+
+def PrintBoard(Board):
+    print()
+    print('The Board Looks Like this')
+    print()
+    for i in range(3):
+        print(' | ' + Board[i][0] + ' | ' + Board[i][1]  + ' | ' + Board[i][2] + ' | ')
 
 Board = CreateBoard()
 Player = 'X'
 Board[0][0] = Player
 Board[0][1] = Player
+Board[0][2] = Player
+PrintBoard(Board)
 horizontals = CheckHor(Board, Player)
 verticals = CheckVer(Board, Player)
+Diags = CheckDiag(Board, Player)
 print(horizontals)
 print(verticals)
+print(Diags)
