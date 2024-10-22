9ddb5da7747e65cf9c2adfc10e5b5782ec323ebf
Jarrell-Wi <156929253+Jarrell-Wi@users.noreply.github.com>
2024-10-22 08:50:12 +0000

    Tetris start

--- Projects/Tetris Concept/Main.py
+++ Projects/Tetris Concept/Main.py
@@ -1,6 +1,6 @@
 def CreateBoard():
     Board = []
-    Part = ' '
+    Part = '   '
     for i in range(24):
         Row = []
         for j in range(10):
@@ -11,8 +11,8 @@ def CreateBoard():
 def PrintBoard(Board):
     for i in range(24):
         print(' |' + Board[i][0] + '|' + Board[i][1]  + '|' + Board[i][2] + '|' + Board[i][3] + '|' + Board[i][4] + '|' + Board[i][5] + '|' + Board[i][6] + '|' + Board[i][7] + '|'+ Board[i][8] + '|' + Board[i][9] + '|')
-
+    return
 def Start():
-    Board = CreateBoard
+    Board = CreateBoard()
     PrintBoard(Board)
 Start()
